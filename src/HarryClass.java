2afea0cfa600737e95e95f7dc4761fafd2370468
mosharrofrubel <rubel007cse@gmail.com>
2016-10-26 16:39:35 +0600

    Inheritance is working fine

---
+++ src/HarryClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created by mosharrofrubel on 10/26/16.
+ */
+public class HarryClass extends OsbornNorman{
+
+    public static void main(String[] args){
+
+        // creating object
+        HarryClass object = new HarryClass();
+
+        // Inheriting properties from OsbornNorman class
+        object.research_of_lifetime();
+        object.diseases_of_harry();
+
+    }
+
+}
---
+++ src/OsbornNorman.java
@@ -0,0 +1,29 @@
+/**
+ * Created by mosharrofrubel on 10/26/16.
+ */
+public class OsbornNorman {
+
+    // a property containing method
+    public void research_of_lifetime() {
+        int papers = 50;
+        String password = "333455";
+        int others = 78;
+        String access_email = "norman_secret_email@yahoo.com";
+
+        // printing all
+        System.out.println("Papers you will get: "+papers+", Password:"+password);
+        System.out.println("Others: "+others+", Email: "+access_email);
+
+    }
+
+
+
+    // another property
+
+    public void diseases_of_harry (){
+        String diseases_name = "Retroviral Hypodisplasia";
+
+        System.out.println("Name of the diseases that Harry will have, "+diseases_name);
+    }
+
+}
